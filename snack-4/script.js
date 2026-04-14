ab4864efb60f052264743ad95896774f523ac463
rAel-Ael <alediru9267@gmail.com>
2026-04-14 17:16:32 +0200

    es 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const nomiAutori = posts.map(posts => posts.author);
+console.log(nomiAutori);
