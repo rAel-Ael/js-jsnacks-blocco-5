238b064fc236d0f2f46f61476900ffa1fa7dda64
rAel-Ael <alediru9267@gmail.com>
2026-04-15 00:25:22 +0200

    es 6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,8 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineLunghe = zucchine.filter(zucchine => zucchine.length >= 15); 
+
+
+console.log(zucchineLunghe);
